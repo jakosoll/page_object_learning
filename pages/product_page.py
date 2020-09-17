32da9971271bdb8d2dd9693176c54685e72109b1
Evgeiy Krivenko <jako.soll@gmail.com>
2020-09-17 14:48:00 +0300

    Added Page Object class for testing add to cart

---
+++ pages/product_page.py
@@ -0,0 +1,12 @@
+from pages.base_page import BasePage
+
+
+class ProductPage(BasePage):
+    def add_product_to_cart(self):
+        pass
+
+    def should_be_message_product_added(self):
+        pass
+
+    def should_be_cart_cart_price(self):
+        pass
