f8c36f2efc712b0542db9d3281d5410caf76d2d4
Evgeiy Krivenko <jako.soll@gmail.com>
2020-09-17 16:05:13 +0300

    Added locators for name, price and button

--- pages/locators.py
+++ pages/locators.py
@@ -10,3 +10,11 @@ class LoginPageLocators:
     LOGIN_FORM_EMAIL_INPUT = (By.CSS_SELECTOR, 'input[type="email"]')
     LOGIN_FORM_PASSWORD_INPUT = (By.CSS_SELECTOR, 'input[type="password"]')
     REGISTRATION_FORM = (By.CSS_SELECTOR, '#register_form')
+
+
+class ProductPageLocators:
+    PRODUCT_NAME = (By.TAG_NAME, 'h1')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, '.price_color')
+    BUTTON_ADD_TO_CART = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    ADDED_MESSAGE = (By.XPATH, '//*[@id="messages"]/div[1]/div/strong')
+    BASKET_PRICE = (By.XPATH, '//*[@id="messages"]/div[3]/div/p[1]/strong')
