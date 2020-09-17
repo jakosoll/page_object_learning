a360dafb7ccad6ab7ab5de923e4076a29dc7890e
Evgeiy Krivenko <jako.soll@gmail.com>
2020-09-17 17:10:27 +0300

    Create ProductPage with methods

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,7 +4,6 @@ from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage:
-    browser: Type[webdriver.Chrome]
 
     def __init__(self, browser: Type[webdriver.Chrome], url, timeout: int = 10):
         self.browser = browser
@@ -16,7 +15,7 @@ class BasePage:
 
     def is_present_element(self, how, what):
         try:
-            self.browser.find_element(how, what)
+            element = self.browser.find_element(how, what)
         except NoSuchElementException:
             return False
-        return True
+        return element
--- pages/product_page.py
+++ pages/product_page.py
@@ -1,12 +1,55 @@
-from pages.base_page import BasePage
+from typing import Type
+from selenium import webdriver
+from .base_page import BasePage
+from .locators import ProductPageLocators
+from selenium.common.exceptions import NoAlertPresentException
+import math
 
 
 class ProductPage(BasePage):
+
+    def __init__(self, browser: Type[webdriver.Chrome], url, product_name=None, product_price=None):
+        super().__init__(browser, url)
+        self.product_name = product_name
+        self.product_price = product_price
+
     def add_product_to_cart(self):
-        pass
+        button_add_to_cart = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_CART)
+        button_add_to_cart.click()
 
-    def should_be_message_product_added(self):
-        pass
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
 
-    def should_be_cart_cart_price(self):
-        pass
+    def get_product_name(self):
+        product_name = self.is_present_element(*ProductPageLocators.PRODUCT_NAME)
+        if product_name:
+            self.product_name = product_name.text
+        else:
+            assert False, 'No product name'
+
+    def get_product_price(self):
+        product_price = self.is_present_element(*ProductPageLocators.PRODUCT_PRICE)
+        if product_price:
+            self.product_price = product_price.text
+        else:
+            assert False, 'No product price'
+
+    def should_be_product_added_message(self):
+        product_added_message = self.browser.find_element(*ProductPageLocators.ADDED_MESSAGE).text
+        assert self.product_name == product_added_message, \
+            'Product name != Product name in basket'
+
+    def should_be_basket_price(self):
+        basket_price = self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
+        assert self.product_price == basket_price, 'Basket price != product price'
