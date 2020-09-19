3c86f576260b5276d90e6f46f6e4032aecbc7ddc
Евгений <jako.soll@gmail.com>
2020-09-19 17:42:56 +0300

    Updated links. Deleted some language's elements

--- test_product_page.py
+++ test_product_page.py
@@ -17,14 +17,14 @@ class TestUserAddToBasketFromProductPage:
         registration_page.should_be_authorized_user()
 
     def test_user_cant_see_success_message(self, browser):
-        link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
         page = ProductPage(browser, link)
         page.go_to_site()
         page.should_not_be_success_message()
 
     @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
-        link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
         page = ProductPage(browser, link)
         page.go_to_site()
         page.get_product_name()
@@ -36,7 +36,7 @@ class TestUserAddToBasketFromProductPage:
 
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_form_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.go_to_site()
     page.go_to_login_page()
@@ -44,7 +44,7 @@ def test_guest_can_go_to_login_page_form_product_page(browser):
 
 @pytest.mark.need_review
 def test_guest_can_see_product_in_basket_opened_from_product_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/'
     page = ProductPage(browser, link)
     page.go_to_site()
     page.go_to_basket()
@@ -54,7 +54,7 @@ def test_guest_can_see_product_in_basket_opened_from_product_page(browser):
 
 
 def test_guest_cant_see_success_message(browser):
-    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
     page.go_to_site()
     page.should_not_be_success_message()
@@ -62,7 +62,7 @@ def test_guest_cant_see_success_message(browser):
 
 @pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
     page.go_to_site()
     page.get_product_name()
@@ -74,7 +74,7 @@ def test_guest_can_add_product_to_basket(browser):
 
 @pytest.mark.xfail
 def test_guest_can_see_success_message_after_product_adding(browser):
-    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
     page.go_to_site()
     page.add_product_to_cart()
@@ -83,7 +83,7 @@ def test_guest_can_see_success_message_after_product_adding(browser):
 
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
     page.go_to_site()
     page.add_product_to_cart()
@@ -91,7 +91,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
 
 
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.go_to_site()
     page.should_be_login_link()
